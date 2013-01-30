feb2f6649738f25623ddc3a7475ce8e3d2a9531e
Jose Santos <jsmpereira@gmail.com>
2013-01-30 01:08:59 +0000

    Use coleslaw heroku plugin. Fix load-config redefine and main funcall.

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -1,6 +1,6 @@
 (in-package :cl-user)
 
-(print ">>> Setting up web server....\n")
+(print ">>> Setting up Coleslaw Lisp Blogware....")
 
 ;; *build-dir* is set from Heroku env variable BUILD_DIR, on heroku-buildpack-cl
 
@@ -11,8 +11,9 @@
 
 ;; Posts live in our heroku repo under /posts.
 ;; Also place .coleslawrc on our repository root.
-(defun load-config ()
+(defun load-config (config-key)
   "Load the coleslaw configuration from DIR/.coleslawrc. DIR is ~ by default."
+  (declare (ignore config-key))
   (with-open-file (in (merge-pathnames ".coleslawrc" cl-user::*build-dir*))
     (setf *config* (apply #'make-instance 'blog (read in))))
   (load-plugins (plugins *config*)))
@@ -27,42 +28,6 @@
         (run-program "ln -sfn ~a ~a" (rel-path #P"/app/" (normalize target)) path))))
 
 ;; Create our blog
-(coleslaw:main)
+(coleslaw:main nil)
 
-;;; Hunchentoot
-(in-package :hunchentoot)
-;; Hunchentoot fix for (create-folder-dispatcher-and-handler) with uri-prefix "/", should be available in next ql release.
-(defun parse-path (path)
-  "Return a relative pathname that has been verified to not contain
-  any directory traversals or explicit device or host fields.  Returns
-  NIL if the path is not acceptable."
-  (when (every #'graphic-char-p path)
-    (let* ((pathname (pathname (remove #\\ (regex-replace "^/*" path ""))))
-           (directory (pathname-directory pathname)))
-      (when (and (or (null (pathname-host pathname))
-                     (equal (pathname-host pathname) (pathname-host *default-pathname-defaults*)))
-                 (or (null (pathname-device pathname))
-                     (equal (pathname-device pathname) (pathname-device *default-pathname-defaults*)))
-                 (or (null directory)
-                     (and (eql (first directory) :relative)
-                          (every #'stringp (rest directory))))) ; only string components, no :UP traversals
-        pathname))))
-
-(defun request-pathname (&optional (request *request*) drop-prefix)
-  "Construct a relative pathname from the request's SCRIPT-NAME.
-If DROP-PREFIX is given, pathname construction starts at the first path
-segment after the prefix.
-"
-  (let ((path (url-decode (script-name request))))
-    (if drop-prefix
-        (when (starts-with-p path drop-prefix)
-          (parse-path (subseq path (length drop-prefix))))
-        (parse-path path))))
-
-;; Tell hunchentoot where to serve from and the point of entry
-(push (create-folder-dispatcher-and-handler "/" "/app/.curr/")
-      *dispatch-table*)
-(push (create-static-file-dispatcher-and-handler "/" "/app/.curr/index.html")
-      *dispatch-table*)
-
-(print ">>> Done setting up web server.")
+(print ">>> Done setting up Coleslaw Lisp Blogware.")
